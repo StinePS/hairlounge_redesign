5b825c9a7326eea12930a41fa28d3f499e165c36
Johanne Skou Karlsen <Johanne@johannes-mbp.home>
2020-12-15 20:47:59 +0100

    slide om os

---
+++ js/omos.js
@@ -0,0 +1,30 @@
+//Siden er loadet
+window.addEventListener("load", siteLoaded);
+
+
+let slide = 2;
+
+
+
+function siteLoaded() {
+    console.log("side er loadet");
+    document.querySelector("#text2").classList.remove("show");
+    document.querySelector("#text2").classList.add("hide");
+    document.querySelector("#text1").classList.add("show");
+
+    document.querySelector("#knap_right").classList.remove("opacity");
+    document.querySelector("#knap_left").classList.add("opacity");
+    document.querySelector("#knap_right").addEventListener("click", rightslide);
+}
+
+function rightslide() {
+    console.log("klik");
+    document.querySelector("#text1").classList.remove("show");
+    document.querySelector("#text1").classList.add("hide");
+
+    document.querySelector("#knap_left").classList.remove("opacity");
+     document.querySelector("#knap_right").classList.add("opacity");
+
+    document.querySelector("#text2").classList.add("show");
+    document.querySelector("#knap_left").addEventListener("click", siteLoaded);
+}
